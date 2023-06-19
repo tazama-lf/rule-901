8f4bcc1f32983303b58aa033da9aea2d41a5c19f
JFoley <johannes.foley@sybrin.com>
2023-06-19 16:28:02 +0200

    BUG: Add tests fix endless run test commands
    Fix the endless looping test script.

---
+++ __tests__/rule-901.test.ts
@@ -0,0 +1,7 @@
+import { assert } from "console";
+
+describe('Logic Service', () => {
+    it('should respond with rule result of true for happy path', async () => {
+        expect(true).toEqual(true);
+    });
+});
