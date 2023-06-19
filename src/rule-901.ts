2d59c19a367fb764a73a220b3c1bae1d8cd0977a
JFoley <johannes.foley@sybrin.com>
2023-06-19 08:53:40 +0200

    FEAT: Create sample rule to count all transactions from Debtor
    Create sample rule to count all transactions from Debtor to be used as sample to the Rule Executor.

---
+++ src/rule-901.ts
@@ -0,0 +1,35 @@
+import { DatabaseManagerInstance, LoggerService, ManagerConfig } from '@frmscoe/frms-coe-lib';
+import { DataCache, RuleConfig, RuleRequest, RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
+
+export async function handleTransaction(
+    req: RuleRequest,
+    determineOutcome: (value: number, ruleConfig: RuleConfig, ruleResult: RuleResult) => RuleResult,
+    ruleRes: RuleResult,
+    loggerService: LoggerService,
+    ruleConfig: RuleConfig,
+    databaseManager: DatabaseManagerInstance<ManagerConfig>,
+    dataCache: DataCache
+): Promise<RuleResult> {
+    loggerService.log("Rule Received request", "handleTransaction");
+    const transactionAmount = await (await databaseManager._pseudonymsDb.query(`
+        FOR 
+            doc
+        IN 
+            transactionRelationship
+        FILTER
+            doc.TxTp=="pacs.002.001.12"
+            AND doc._from=="accounts/${dataCache.dbtrAcctId}"
+        COLLECT WITH COUNT INTO length
+        RETURN 
+            length
+    `)).batches.all();
+    if (!transactionAmount || !transactionAmount[0])
+        return ruleRes;
+
+    const outcome = await determineOutcome(transactionAmount[0][0], ruleConfig, ruleRes);
+    ruleRes.reason = outcome.reason;
+    ruleRes.result = outcome.result;
+    ruleRes.subRuleRef = outcome.subRuleRef;
+    loggerService.log(`Rule ${ruleRes.id}@${ruleRes.cfg} processed with outcome: ${ruleRes.subRuleRef}`);
+    return ruleRes;
+}
