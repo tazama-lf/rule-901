a3493670ca48ddb43da906f4fbe871e3c3003ca4
JFoley <johannes.foley@sybrin.com>
2023-06-19 09:56:45 +0200

    BUG: Added error handling
    Added some guard statements to throw known errors.

--- src/rule-901.ts
+++ src/rule-901.ts
@@ -11,6 +11,9 @@ export async function handleTransaction(
     dataCache: DataCache
 ): Promise<RuleResult> {
     loggerService.log("Rule Received request", "handleTransaction");
+    if(!ruleConfig.config.timeframes[0].threshold) throw new Error("Config Threshold not specified");
+    if(!dataCache.dbtrAcctId) throw new Error("Data Cache does not have required dbtrAcctId");
+
     const transactionAmount = await (await databaseManager._pseudonymsDb.query(`
         FOR 
             doc
@@ -19,12 +22,13 @@ export async function handleTransaction(
         FILTER
             doc.TxTp=="pacs.002.001.12"
             AND doc._from=="accounts/${dataCache.dbtrAcctId}"
+            AND DATE_DIFF(DATE_TIMESTAMP(doc.CreDtTm), DATE_NOW(), "millisecond", false) <= ${ruleConfig.config.timeframes[0].threshold}
         COLLECT WITH COUNT INTO length
         RETURN 
             length
     `)).batches.all();
-    if (!transactionAmount || !transactionAmount[0])
-        return ruleRes;
+    if (!transactionAmount || !transactionAmount[0] || transactionAmount[0][0])
+        throw new Error("Error while retrieving transaction history information");
 
     const outcome = await determineOutcome(transactionAmount[0][0], ruleConfig, ruleRes);
     ruleRes.reason = outcome.reason;
